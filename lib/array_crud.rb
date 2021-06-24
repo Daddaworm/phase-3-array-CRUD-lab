ad542dd0f810452a609fcfce9acaccde91e65c5f
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-24 19:01:16 -0400

    commit tree 5b799d320ed684cd0e04a2ee4918db7eff619775

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Ethan", "Lea", "Ezra", "Mouse Lemur"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    ["wow", "I", "am", "really", "learning"]<<"arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    ["I", "am", "really", "learning"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    ["I", "am", "really", "learning", "arrays!"].pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    ["wow", "I", "am", "really", "learning", "arrays!"].shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    ["wow", "I", "am", "really", "learning", "arrays!"].delete_at(2)
 end
 
 def retrieve_first_element_from_array(array)
-  
+    ["wow", "I", "am", "really", "learning", "arrays!"].first()
 end
 
 def retrieve_last_element_from_array(array)
-  
+    ["wow", "I", "am", "really", "learning", "arrays!"].last()
 end
